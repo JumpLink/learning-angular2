d8bca2062c0c83298228114e62bd50d736f7041e
Pascal Garber <pascal@jumplink.eu>
2016-01-23 23:56:51 +0100

    minify and generate sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
+var webpack = require('webpack');
 module.exports = {
     entry: "./entry.ts",
     output: {
         path: __dirname,
         filename: "bundle.js"
     },
+    // Turn on sourcemaps
+    devtool: 'source-map',
     resolve: {
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
     },
+    // Add minification
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ],
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" },
